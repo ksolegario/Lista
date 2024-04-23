71428a40bb088f78bbbadd943e473a6d4173324e
20221tiimi0190 <kalebe.olegario1850@gmail.com>
2024-04-23 09:11:04 -0300

    Lista em andamento...

--- app/src/main/java/olegario/kalebe/lista/activity/NewItemActivity.java
+++ app/src/main/java/olegario/kalebe/lista/activity/NewItemActivity.java
@@ -1,4 +1,4 @@
-package olegario.kalebe.lista;
+package olegario.kalebe.lista.activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import olegario.kalebe.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_item);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
