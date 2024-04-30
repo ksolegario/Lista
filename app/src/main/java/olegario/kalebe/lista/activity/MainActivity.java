f5c9d905440a8f3549da97b1c1c6b7c20604b207
20221tiimi0190 <kalebe.olegario1850@gmail.com>
2024-04-30 09:13:34 -0300

    Lista em andamento...

--- app/src/main/java/olegario/kalebe/lista/activity/MainActivity.java
+++ app/src/main/java/olegario/kalebe/lista/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package olegario.kalebe.lista.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +10,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import olegario.kalebe.lista.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    static int NEW_ITEM_REQUEST =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        FloatingActionButton fabAddItem = findViewById(R.id.fabAddNewItem);
+        fabAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, NewItemActivity.class);
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
 }
--- app/src/main/java/olegario/kalebe/lista/activity/NewItemActivity.java
+++ app/src/main/java/olegario/kalebe/lista/activity/NewItemActivity.java
@@ -1,8 +1,19 @@
 package olegario.kalebe.lista.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,15 +23,59 @@ import olegario.kalebe.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
 
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new_item);
+
+        ImageButton imgCI = findViewById(R.id.imgCI);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
+
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btnAddItem = findViewById(R.id.btnAddItem);
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (photoSelected == null) {
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG) .show();
+                    return;
+                }
+
+                EditText editText = findViewById(R.id.etTitle);
+                String title = editText.getText(). toString();
+                if (title.isEmpty()) {
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG);
+                    return;
+                }
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PHOTO_PICKER_REQUEST) {
+            if (resultCode == Activity.RESULT_OK) {
+                photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvfotoPreview);
+                imvfotoPreview.setImageURI(photoSelected);
+            }
+        }
+    }
+
 }
